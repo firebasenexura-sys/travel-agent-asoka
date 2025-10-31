fa66a038369d9e36496371048be4c64a3c448a2c
RyuuMi <dienghebat.nexura@gmail.com>
2025-11-01 01:49:20 +0700

    cleanup: remove unused measurementId

--- lib/firebase/config.ts
+++ lib/firebase/config.ts
@@ -8,13 +8,12 @@ import { getAuth } from "firebase/auth";
 
 // Catatan: Ganti dengan konfigurasi proyek Firebase Anda yang sudah disimpan
 const firebaseConfig = {
-  apiKey: "AIzaSyBdKqLcrk_iA_8kXCVEcN3r5uDBiZTly3Y",
-  authDomain: "travelagent-asoka.firebaseapp.com",
-  projectId: "travelagent-asoka",
-  storageBucket: "travelagent-asoka.firebasestorage.app",
-  messagingSenderId: "1083864482880",
-  appId: "1:1083864482880:web:25cbd9961295d9c3900ba5",
-  measurementId: "G-SYV591R20B"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Inisialisasi Firebase
@@ -24,5 +23,5 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
-
+export default app;
 // Selesai!
